c6b24b1f6aeac6bc539dd9385639dfe8e732db8f
Alex <Alex@MacBook-Alex.local>
2015-12-28 14:07:33 +0100

    maj

--- get_next_line.c
+++ get_next_line.c
@@ -18,44 +18,44 @@ char    *ft_allo_mem(char *tab, int size, char buf)
 	char    *tab2;
 	int     x;
 
-	x = 0;
-	tab2 = (char*)malloc((1 * size) + 1);
-	while (x < size)
-	{
+	x = -1;
+	if ((tab2 = (char*)malloc((1 * size) + 1)) == NULL)
+		return (NULL);
+	while (++x < size)
 		tab2[x] = tab[x];
-		x++;
-	}
 	tab2[x] = buf;
 	return (tab2);
 }
 
 int		get_next_line(int const fd, char **line)
 {
+  	static	int	sta = 0;
 	int		i;
 	int		x;
-	char	*tab;
+	int		ret;
+	int		test;
 	char	buf[BUFF_SIZE + 1];
 
 	i = 0;
 	x = 0;
-	line = NULL;
-	tab = (char *)malloc(sizeof(tab) * (1 + 1));
-	if (!tab)
-		return (0);
+	test = 0;
 	if (fd == -1)
 		return (-1);
-	while (read(fd, buf, BUFF_SIZE))
+	while ((ret = read(fd, buf, BUFF_SIZE)) && ret != -1 && test != 1)
 	{
 		i = 0;
-		while (buf[i] != '\n')
+		while (buf[i] != '\n' && i < BUFF_SIZE && buf[i])
 		{
-			tab = ft_allo_mem(tab, x, buf[i]);
+			line[sta] = ft_allo_mem(line[sta], x, buf[i]);
 			i++;
 			x++;
+			if (buf[i] == '\n')
+				test = 1;
 		}
 	}
-	ft_putstr(tab);
-	return (0);
+	sta++;
+	ft_putnbr(sta);
+	return (ret);
 }
 
 int	main(int ac, char **av)
@@ -63,11 +63,15 @@ int	main(int ac, char **av)
 	int		fd;
 	char	**line;
 
-	line = NULL;
+	line = (char **)malloc(sizeof(line) * 10);
+	if (!line)
+		return (0);
 	ac = ac * 1;
 	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
-		return (0);
 	get_next_line(fd, line);
+	ft_putstr(line[0]);
+	ft_putchar('\n');
+	get_next_line(fd, line);
+	ft_putstr(line[1]);
 	return (0);
 }
