a4d1cac6df5f2d75ddcfc5729eadc63ef3f3620a
Alex MOINIER <amoinier@e2r8p5.42.fr>
2016-01-08 19:05:27 +0100

    GNL FINISH

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amoinier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/19 17:02:06 by amoinier          #+#    #+#             */
-/*   Updated: 2016/01/08 17:34:43 by amoinier         ###   ########.fr       */
+/*   Updated: 2016/01/08 19:04:21 by amoinier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,22 @@ static	int		ft_stockfile(int const fd, char *tmp[fd], char *buf)
 	return (ret);
 }
 
+static	int		ft_space(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
 int				get_next_line(int const fd, char **line)
 {
-	int				i;
 	char			*buf;
 	char			*tt;
 	static	char	*tmp[BUFF_SIZE];
@@ -51,39 +64,17 @@ int				get_next_line(int const fd, char **line)
 		return (-1);
 	if (ft_stockfile(fd, &(*tmp), buf) < 0)
 		return (-1);
-	if (*tmp[fd] == '\0')
+	if (tmp[fd][0] == '\0')
 	{
 		*line = NULL;
 		return (0);
 	}
-	i = 0;
-	while (tmp[fd][i] != '\n' && tmp[fd][i])
-		i++;
-	*line = ft_strsub(tmp[fd], 0, i);
-	tt = (char *)malloc(sizeof(tt) * (ft_strlen(tmp[fd]) + 1));
-	ft_strcpy(tt, &tmp[fd][i + 1]);
-	tmp[fd] = tt;
+	*line = ft_strsub(tmp[fd], 0, ft_space(tmp[fd]));
+	if (!(tt = (char *)malloc(sizeof(tt) * (ft_strlen(tmp[fd]) + 1))))
+		return (-1);
+	ft_strcpy(tt, &tmp[fd][ft_space(tmp[fd]) + 1]);
+	ft_strclr(tmp[fd]);
+	tmp[fd] = ft_strcpy(tmp[fd], tt);
 	free(tt);
 	return (1);
 }
-/*
-int		main(int ac, char **av)
-{
-	int		fd;
-	int		i;
-	char	*line;
-
-	line = NULL;
-	ac = ac * 1;
-	fd = open(av[1], O_RDONLY);
-	while ((i = get_next_line(fd, &line)) == 1)
-	{
-		ft_putnbr(i);
-		ft_putchar('|');
-		ft_putstr(line);
-		ft_putchar('|');
-		ft_putchar('\n');
-	}
-	return (0);
-}
-*/
