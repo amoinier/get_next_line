bd56d07904716d856fdf8f027eecc9a1933c7c8b
Alex MOINIER <amoinier@e2r9p5.42.fr>
2016-01-12 12:39:40 +0100

    probleme buff_size

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: amoinier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/19 17:02:06 by amoinier          #+#    #+#             */
-/*   Updated: 2016/01/08 19:04:21 by amoinier         ###   ########.fr       */
+/*   Updated: 2016/01/12 12:37:19 by amoinier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,16 @@ static	char	*ft_strjoin_free(char *s1, char *s2)
 	return (s1);
 }
 
-static	int		ft_stockfile(int const fd, char *tmp[fd], char *buf)
+static	int		ft_stockfile(int const fd, char *tmp[fd])
 {
 	int		ret;
+	char	buf[BUFF_SIZE + 1];
 
-	while (!(ft_strchr(buf, '\n')) && (ret = read(fd, buf, BUFF_SIZE)) > 0)
+	while (!(ft_strchr(tmp[fd], '\n')) && (ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
 		tmp[fd] = ft_strjoin_free(tmp[fd], buf);
 	}
-	free(buf);
-	buf = NULL;
 	return (ret);
 }
 
@@ -53,16 +52,14 @@ static	int		ft_space(char *s)
 
 int				get_next_line(int const fd, char **line)
 {
-	char			*buf;
 	char			*tt;
-	static	char	*tmp[BUFF_SIZE];
+	static	char	*tmp[1];
 
-	if (fd < 0 || !line ||
-	(!(buf = (char *)malloc(sizeof(buf) * (BUFF_SIZE + 1)))))
+	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
 	if (!tmp[fd] && (!(tmp[fd] = (char *)malloc(sizeof(tmp[fd]) * (1 + 1)))))
 		return (-1);
-	if (ft_stockfile(fd, &(*tmp), buf) < 0)
+	if (ft_stockfile(fd, &(*tmp)) < 0)
 		return (-1);
 	if (tmp[fd][0] == '\0')
 	{
